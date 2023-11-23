c8c937448de0e5017d3ba4e3fe88d6fec7ea269c
barancinar <91697327+barancinar@users.noreply.github.com>
2023-11-24 00:06:12 +0300

    Timeline

---
+++ main/java/com/barancinar/timeline/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.barancinar.timeline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
